c092124f3bb643b04df7ca40f8ef6bb08a48ac3a
bimo.henokh <bimo.henokh@ui.ac.id>
2022-09-22 15:41:50 +0700

    lab3

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -4,6 +4,10 @@ from wishlist.views import receive_request
 from wishlist.views import request_json
 from wishlist.views import show_xml_by_id
 from wishlist.views import show_json_by_id
+from wishlist.views import register #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from wishlist.views import logout_user #sesuaikan dengan nama fungsi yang dibuat
+
 
 app_name = 'wishlist'
 
@@ -13,4 +17,7 @@ urlpatterns = [
     path('json/', request_json, name='request_json'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'), #sesuaikan dengan nama fungsi yang dibuat
 ]
